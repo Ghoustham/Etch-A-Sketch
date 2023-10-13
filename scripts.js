8084f81d00f711908230e29cf519d4ced0c24116
Ghoustham <javier.aguiht@gmail.com>
2023-10-13 15:26:20 -0300

    The function createSquares has been caaeted

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 console.log('hala world')
+let num = 1;
+
+const createSquares = (num)=> {
+
+    const divSelector = document.querySelector('div');
+
+    for(let i= 0; i <= num;i++ ){
+        const newSquare = document.createElement('div');
+        newSquare.classList.add('squares');
+
+        divSelector.append(newSquare);
+    }
+    
+
+
+
+
+
+}
+
+createSquares(num);
