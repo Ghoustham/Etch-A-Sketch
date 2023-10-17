8e28e47205c86a399d53d6687dc7c8fbc8d1194e
Ghoustham <javier.aguiht@gmail.com>
2023-10-16 22:33:04 -0300

    The layout of squares and the prompt for the user has been created

--- scripts.js
+++ scripts.js
@@ -1,22 +1,34 @@
 console.log('hala world')
-let num = 1;
+let num = 0 ;
+
+const playerNumber = ()=> {
+    const takeNumberPLayer = prompt('Please enter a number from 1 to 100 for the numbers of squares');
+    const sizeOfGrid = takeNumberPLayer;
+    return sizeOfGrid;
+}
+
+num = playerNumber();
 
 const createSquares = (num)=> {
-
-    const divSelector = document.querySelector('div');
-
-    for(let i= 0; i <= num;i++ ){
-        const newSquare = document.createElement('div');
-        newSquare.classList.add('squares');
-
-        divSelector.append(newSquare);
-    }
     
-
-
-
-
-
+    const divSelector = document.querySelector('.container');
+    divSelector.style.gridTemplateColumns = `repeat(${num},1fr)`;
+    divSelector.style.gridTemplateRows = `repeat(${num},1fr)`;
+    num = num * num;
+    
+    for(let i= 0; i < num;i++ ){
+        
+        const newSquare = document.createElement('div');
+        
+        newSquare.classList.add('squares');
+        
+        divSelector.append(newSquare);
+        
+    }
 }
 
 createSquares(num);
+
+// Create a function that when i hover the squares they changes the color of their background 
+
+
